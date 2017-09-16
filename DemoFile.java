7e5f374b2cff9a030e81b8a3f0e718b130012782
RajivChowdhury <onsitesimplyrajiv@gmail.com>
2017-09-16 11:34:34 -0700

    Update DemoFile.java

--- DemoFile.java
+++ DemoFile.java
@@ -13,12 +13,6 @@ public void multiply(int a, int b){
 	pass;
 }
 
-<<<<<<< HEAD
 public void squareroot(int a, int b){
 	pass;
 }
-=======
-public void divide(int a, int b){
-	pass;	
-}
->>>>>>> 6d82801e4513194c6cbc5dc5b64c77d2fa2b51c7
