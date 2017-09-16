b22eeb64e10ba957418d83e033cf6880d18ec65a
RajivChowdhury <onsitesimplyrajiv@gmail.com>
2017-09-16 14:11:57 -0700

    x

--- DemoFile.java
+++ DemoFile.java
@@ -17,33 +17,5 @@ public void squareroot(int a, int b){
 	pass;
 }
 
-public void Func2(int a, int b){
-	pass;
-}
 
-public void Func1(int a, int b){
-	pass;
-}
-
-public void Func3(int a, int b){
-	pass;
-}
-
-
-public void Func4(int a, int b){
-}
-public void Func5(int a, int b){
-
-	pass;
-}
-
-public void Func6(int a, int b){
-
-	pass;
-}
-
-public void Func7(int a, int b){
-
-	pass;
-}
 
